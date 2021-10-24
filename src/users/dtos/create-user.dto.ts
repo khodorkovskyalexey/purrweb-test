a217db67cace57fe8cfb9d3b7827b6f4071b9761
terminator_79 <aezz120813@gmail.com>
2021-10-25 01:07:15 +0600

    feat: add user validation

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsString, Length } from "class-validator"
+
 export class CreateUserDto {
+  @IsString({ message: "Must be string" })
+  @IsEmail({}, { message: "Must be email" })
   readonly email: string
+
+  @IsString({ message: "Must be string" })
+  @Length(4, 16, { message: "Must be from 4 to 16" })
   readonly password: string
 }
